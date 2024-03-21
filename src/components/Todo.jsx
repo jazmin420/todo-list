1ba659e21898d1fa6335adea8e3bc51f0c11573e
Jazmin420 <jashirajasmim@gmail.com>
2024-03-21 13:15:34 +0530

    button

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -12,8 +12,8 @@ function Todo({ id, text, completed })  {
    <div style={{backgroundColor:'whitesmoke'}} className='d-flex justify-content-around align-items-center'>
       <input style={{width: '20px', height: '20px'}} type="checkbox" checked={completed} onChange={() => dispatch(actions.toggleTodo(id))} />
       <span style={{fontSize: '24px', padding: '20px'}}>{text}</span>
-      <button style={{border: 'none'}} onClick={() => dispatch(actions.deleteTodo(id))}>
-        <i className='fa-solid fa-trash text-danger'></i>
+      <button className='btn' style={{border: 'none', color: 'red'}} onClick={() => dispatch(actions.deleteTodo(id))}>
+        Delete
       </button>
     </div>
     </>
